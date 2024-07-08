191e8a6bf9e7e29799936eec301e506be7989894
Nya! <93830081+wzfdgh@users.noreply.github.com>
2024-07-08 11:18:25 +0800

    Update

--- Tool/certificate-gen.sh
+++ Tool/certificate-gen.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #https://www.mingfer.cn/2020/06/13/altern-name
 echo "INFO: 清理环境"
-rm *.rsa *.jks *.p12 *.key *.csr *.srl
+rm *.rsa *.jks *.p12 *.csr *.srl
 
 cat > ca.cnf <<EOF
 [req]
@@ -32,39 +32,6 @@ organizationName = Organization Name (eg, company)
 commonName = Common Name (eg, name)
 commonName_max = 64
 
-####################################################################
-[ ca ]
-default_ca = CA_default
-
-####################################################################
-[ CA_default ]
-
-dir = .
-certs = $dir
-crl_dir = $dir
-database = $dir/index.txt
-#unique_subject = no
-
-new_certs_dir = $dir
-
-certificate = $dir/ca.pem
-serial      = $dir/serial
-crlnumber   = $dir/crlnumber
-
-crl     = $dir/crl.pem
-private_key = $dir/private/cakey.pem
-RANDFILE    = $dir/private/.rand
-
-x509_extensions = usr_cert
-
-name_opt = ca_default
-cert_opt = ca_default
-
-default_days = 36500
-default_crl_days = 30
-default_md = default
-preserve = no
-
 policy = policy_anything
 [ policy_anything ]
 countryName = optional
@@ -78,24 +45,34 @@ emailAddress = optional
 [v3_req]
 basicConstraints = CA:FALSE
 keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage = clientAuth, serverAuth
 subjectAltName = @alt_names
 
 [alt_names]
 IP.1 = 127.0.0.1
 EOF
 
-echo "INFO: 生成自签发证书"
-openssl req -x509 -new -newkey rsa:2048 -nodes -keyout ca.key -out ca.pem -config ca.cnf -days 36500 -extensions v3_req
+if [ -e ./ca.key ]; then
+  echo "INFO: 无需生成 ca 证书"
+else
+  echo "INFO: 生成 ca 证书"
+  openssl ecparam -genkey -name secp256r1 | openssl ec -out ca.key
+  openssl req -x509 -new -key ca.key -out ca.pem -config ca.cnf -extensions v3_req -days 36500
+fi
 
-echo "INFO: 签发服务端证书"
+echo "INFO: 生成 server 证书"
 read -p "请输入服务器域名或者主机名：" server
+read -p "请输入证书文件名：" cert
 echo "INFO: set alt_names $server"
 old_server=$(grep "IP.1 = " server.cnf|awk -F " " '{print $3}')
 echo "INFO: 将 alt_names 从 $old_server 修改为 $server"
 sed -i "s/$old_server/$server/g" server.cnf
-openssl genrsa -out server.key 2048
-openssl req -new -key server.key -out server.csr -config server.cnf
-openssl x509 -req -CA ca.pem -CAkey ca.key -CAcreateserial -in server.csr -out server.pem -extensions v3_req -extfile server.cnf -days 36500
+openssl ecparam -genkey -name secp256r1 | openssl ec -out "$cert".key
+openssl req -new -key "$cert".key -out "$cert".csr -config server.cnf
+openssl x509 -req -CA ca.pem -CAkey ca.key -CAcreateserial -in "$cert".csr -out "$cert".pem -extensions v3_req -extfile server.cnf -days 36500
+
+echo "INFO: 合并证书链"
+cat ca.pem >> "$cert".pem
 
 echo "INFO：清理无用文件"
 rm *.rsa *.csr ca.srl *cnf
